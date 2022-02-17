110920cbd4b0ad8ce4db0b3ec8fbffa9f83b9c23
Michael Hranek <mhranek@comcast.net>
2022-02-17 17:04:25 -0500

    Added space hero image

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Navigate({ currentPage, handlePageChange }) {
     return (
-        <nav className='bg-info pt-5 border-bottom border-5 border-success fs-4 mb-5'>
+        <nav className='pt-5 border-bottom border-5 border-info fs-4 mb-5' id='hero-image'>
             <ul className='nav nav-tabs justify-content-center border-0'>
                 <li className='nav-item'>
                     <a
                         href='#About'
                         onClick={() => handlePageChange('About')}
-                        className={`${currentPage === 'About' ? 'nav-link active bg-success border-0' : 'nav-link'} text-dark`}
+                        className={`${currentPage === 'About' ? 'nav-link active bg-info border-0' : 'nav-link'} mx-2`}
                     >
                         About Me
                     </a>
@@ -17,7 +17,7 @@ function Navigate({ currentPage, handlePageChange }) {
                     <a
                         href='#Portfolio'
                         onClick={() => handlePageChange('Portfolio')}
-                        className={`${currentPage === 'Portfolio' ? 'nav-link active bg-success border-0' : 'nav-link'} text-dark`}
+                        className={`${currentPage === 'Portfolio' ? 'nav-link active bg-info border-0' : 'nav-link'} mx-2`}
                     >
                         Portfolio
                     </a>
@@ -26,7 +26,7 @@ function Navigate({ currentPage, handlePageChange }) {
                     <a
                         href='#Contact'
                         onClick={() => handlePageChange('Contact')}
-                        className={`${currentPage === 'Contact' ? 'nav-link active bg-success border-0' : 'nav-link'} text-dark`}
+                        className={`${currentPage === 'Contact' ? 'nav-link active bg-info border-0' : 'nav-link'} mx-2`}
                     >
                         Contact Me
                     </a>
@@ -35,7 +35,7 @@ function Navigate({ currentPage, handlePageChange }) {
                 <a
                         href='#Resume'
                         onClick={() => handlePageChange('Resume')}
-                        className={`${currentPage === 'Resume' ? 'nav-link active bg-success border-0' : 'nav-link'} text-dark`}
+                        className={`${currentPage === 'Resume' ? 'nav-link active bg-info border-0' : 'nav-link'} mx-2`}
                     >
                         Resume
                     </a>
