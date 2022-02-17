20175b20847209e4d4eb4d9c15d9a4d1065cd554
Michael Hranek <mhranek@comcast.net>
2022-02-17 17:30:22 -0500

    Style changes, updated README

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const styles = {
+    headerName: {
+        color: '#3DB6FF',
+        backgroundColor: '#4D4D4D',
+        padding: '10px',
+        borderRadius: '5px',
+        width: 'fit-content'
+    }
+}
+
 function Navigate({ currentPage, handlePageChange }) {
     return (
         <nav className='pt-5 border-bottom border-5 border-info fs-4 mb-5' id='hero-image'>
+            <h1 style={styles.headerName}>Michael Hranek</h1>
             <ul className='nav nav-tabs justify-content-center border-0'>
                 <li className='nav-item'>
                     <a
