f4961f02c1b9690ff98210830640b52cca44efab
Michael Hranek <mhranek@comcast.net>
2022-03-09 20:18:55 -0500

    Updated project links to icons, updated resume link

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div>
             <p>
-                I am currently learning web development at UPenn's bootcamp. In the past I went to RIT for game design and development. I first started coding for my high school senior graduation project, where I taught myself a bit of Python. At RIT I learned the basics of HTML and CSS, but primarily focused on C#. Unfortunately I was unable to complete that program, and spent a few years away from involved coding. I am very excited to be learning it in depth again and am enjoying the time quite a lot.
+                I have just finished UPenn's bootcamp for web development. In the past I went to RIT for game design and development. I first started coding for my high school senior graduation project, where I taught myself a bit of Python. At RIT I learned the basics of HTML and CSS, but primarily focused on C#. Unfortunately I was unable to complete that program, and spent a few years away from coding. I am very excited to have taken UPenn's bootcamp, and am looking forward to using my skills for future projects.
             </p>
         </div>
     )
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Resume() {
     return (
         <div>
-            <a href='https://drive.google.com/file/d/1p2oEWIOxNRL14joaP6UcQ38_nH5Jv-mI/view?usp=sharing' className='btn btn-primary'>My Resume</a>
+            <a href='https://docs.google.com/document/d/1JIbqX0d5G82RrD3LHubpMl6lG1OI_s14fYnRtI6QgTw/edit?usp=sharing' className='btn btn-primary'>My Resume</a>
             <h4>Front-end Languages</h4>
             <p>
                 HTML, CSS, Javascript, Jquery, React
