0c1bd1a04ed9552051e25b68de90c06b2e8b7122
Michael Hranek <mhranek@comcast.net>
2022-03-13 15:03:27 -0400

    updated contact page, links should now open in new tab

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
     return (
         <div>
-            <form className='mx-3'>
+            {/* <form className='mx-3'>
                 <div className='my-3'>
                     <label className='form-label'>Name:</label>
                     <input id="nameInput" className='form-control' type="text"></input>
@@ -17,7 +17,9 @@ function Contact() {
                     <textarea id='messageInput' className='form-control' rows='8'></textarea>
                 </div>
                 <button className='btn btn-success' type="submit">Send</button>
-            </form>
+            </form> */}
+            <h4>mhranekdev@gmail.com</h4>
+            <h4>610 304 6516</h4>
         </div>
     )
 }
